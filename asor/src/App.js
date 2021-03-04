f4fd3646f4cb7daea6867b99fbef36b9ff59f1c3
Shannon Reed <44705472+shanreed@users.noreply.github.com>
2021-03-04 14:59:26 -0800

    added a intro component with styling

--- asor/src/App.js
+++ asor/src/App.js
@@ -2,11 +2,13 @@
 import './App.scss';
 
 import Header from './components/Header';
+import Intro from './components/Intro';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Intro />
     </div>
   );
 }
