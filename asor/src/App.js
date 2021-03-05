febd42e9249cfb52224543b749c5ba1074586757
Shannon Reed <44705472+shanreed@users.noreply.github.com>
2021-03-04 16:17:00 -0800

    added websites and apps carousel with styles

--- asor/src/App.js
+++ asor/src/App.js
@@ -3,12 +3,17 @@ import './App.scss';
 
 import Header from './components/Header';
 import Intro from './components/Intro';
+import WebSites from './components/WebSites';
+import Apps from './components/Apps';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Intro />
+      <WebSites />
+      <Apps />
     </div>
   );
 }
